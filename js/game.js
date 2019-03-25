0575d5edd72054fd1b9e738b15096734d4e857a8
mestock <MelissaStock44@gmail.com>
2019-03-25 13:25:51 -0700

    Added start game button

--- js/game.js
+++ js/game.js
@@ -1,3 +1,10 @@
 'use strict'
 
-console.log('testing!');
+
+//This is where are the start button is being modified
+var startButton = document.getElementById('startButton');
+startButton.addEventListener("click", handleStartGame);
+
+function handleStartGame(){
+    console.log('The game has begun!');
+}
