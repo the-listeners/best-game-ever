7aa8bafbf8e56781f14588fe5e23b03bf0ce1786
Williams Argenal <williamsargenal@gmail.com>
2019-03-25 14:56:08 -0700

    js.

--- js/landing.js
+++ js/landing.js
@@ -1,3 +1,15 @@
 'use strict'
 
 console.log('testing!');
+
+
+var testForm = document.getElementById('user_input');
+var formSubmitHandler = function (formSubmit) {
+  formSubmit.preventDefault();
+  console.log(formSubmit);
+  var userNameForm = formSubmit.target.user_name.value;
+  console.log(userNameForm);
+};
+ testForm.addEventListener('submit', formSubmitHandler);
+
+
