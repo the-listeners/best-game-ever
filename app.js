a286223970147e227cd97c8c7102b7ca53076f1c
Williams Argenal <williamsargenal@gmail.com>
2019-03-25 13:23:18 -0700

    made my form

---
+++ app.js
@@ -0,0 +1,8 @@
+// var formSubmitHandler = function (formSubmit) {
+//   formSubmit.preventDefault();
+//   var target = formSubmit.target.otherKey.value;
+//   var pet = formSubmit.target.key.value;
+
+// };
+// testForm.addEventListener('submit', formSubmitHandler);
+
