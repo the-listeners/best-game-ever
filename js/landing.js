9e54bf65e2833b47f11b5c33814c513ef9e3d670
evanpettie <evanpettie@gmail.com>
2019-03-27 16:51:03 -0700

    aboutme/and misc style

--- js/landing.js
+++ js/landing.js
@@ -32,8 +32,48 @@ storeWordArray();
 // ===========================================================//
 // Toggle about me
 // ===========================================================//
-function aboutMeToggle() {
-  var x = document.getElementById("info");
+function joannaToggle() {
+  var x = document.getElementById("joannaInfo");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+}
+
+function denevanToggle() {
+  var x = document.getElementById("denevanInfo");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+}
+
+function pratiibhToggle() {
+  var x = document.getElementById("pratiibhInfo");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+}
+
+function melissaToggle() {
+  var x = document.getElementById("melissaInfo");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+}
+
+function williamsToggle() {
+  var x = document.getElementById("williamsInfo");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
@@ -63,6 +103,7 @@ function myMove() {
     }
   }
 }
+
 
 
 // redirects when
